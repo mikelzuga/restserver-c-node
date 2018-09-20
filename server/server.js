d63377c0b64af5bbca5263586b037e2f8caa8fe2
Mikel <mikelzuga@hotmail.com>
2018-09-20 14:26:51 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+const colors = require('colors');
+
 const app = express();
+
 const bodyParser = require('body-parser');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,42 +15,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+
+    if (err) throw err;
+    console.log('Base de datos ONLINE'.blue);
 });
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-});
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
-
-//app.listen(3000)
 
 //con callback
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto:', process.env.PORT);
+    console.log('Escuchando puerto:'.blue, process.env.PORT.blue);
 });
