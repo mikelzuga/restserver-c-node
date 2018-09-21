b34f252544fbc4a41dacfbdaacd1f5e908a29172
Mikel <mikelzuga@hotmail.com>
2018-09-21 11:18:38 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://admin:123456q@ds111063.mlab.com:11063/curso_cafe';
+    urlDB = process.env.DB_URL;
 };
-
+// DB_URL = 'mongodb://admin:123456q@ds111063.mlab.com:11063/curso_cafe'
 process.env.URLDB = urlDB;
